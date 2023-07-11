1df6b6a1b6ace19ad1e4e92e99ced3182d92af65
Karan <karanpargal007@gmail.com>
2023-07-11 05:34:07 +0530

    Beautify menu

--- billing_system/src/main.rs
+++ billing_system/src/main.rs
@@ -1,39 +1,35 @@
 use std::io;
+use inquire::Select;
+mod order;
+mod products;
+
+use order::place_order;
+use products::view_products;
 
 fn main() {
     println!("Welcome to the billing system");
     println!("=============================");
-    println!("Menu \n1. Press A to place an order \n2. Press B to view products \n3. Press C to exit");
     menu();
 }
 
 fn menu(){
-    println!("Enter your choice: ");
-    let mut choice = String::new();
-    io::stdin().read_line(&mut choice).expect("Failed to enter choice");
 
-    let option = choice.trim();
+    let options = vec!["Place Order", "See Products", "Exit"];
 
-    if option == "A" {
-        place_order();
-    }
-    else if option == "B" {
-        view_products();
-    }
-    else if option == "C" {
-        println!("Thank you for using the billing system");
-        return;
-    }
-    else {
-        println!("Invalid choice");
-        menu();
-    }
-}
+    let input = Select::new("Menu:", options.clone()).prompt();
 
-fn place_order(){
-    println!("You have placed an order");
-}
-
-fn view_products(){
-    println!("You have viewed the products");
+    match input {
+        Ok(input) => {
+            if input == options[0] {
+                place_order();
+            } else if input == options[1] {
+                view_products();
+            } else if input == options[2] {
+                println!("Thank you for using the billing system");
+            }
+        },
+        Err(_) => {
+            println!("Invalid input");
+        }
+    }
 }
---
+++ billing_system/src/products.rs
@@ -0,0 +1,3 @@
+pub fn view_products(){
+    println!("You have viewed the products");
+}
